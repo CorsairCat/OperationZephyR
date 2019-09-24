4b4064bf7ff8f4c2d10f0b56493ae0c05891b4b4
CorsairCat <51845471+CorsairCat@users.noreply.github.com>
2019-09-24 12:47:37 +0800

    add function of ignore local data

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    ignore_user_abort(TRUE);
     include 'function/getUserCalendar.php';
     include 'function/geneIcal.php';
     include 'function/fileControl.php';
@@ -12,8 +13,8 @@
         $userId = $_GET['userid'];
         if (preg_match('/\d{8}$/',$userId)) {
             $isAlreadyExist = searchData($dataBase,$tableName,1,array(),array("username"),array($userId));
-            if ($isAlreadyExist[0][0] == 0){
-                addData($dataBase,$tableName,1,array("username"),array($userId));
+            if (isset($_GET['ignoreLocal'])){
+                $key = $_GET['ignoreLocal'];
                 $status = writeNewUserArrayToFile($userId);
                 if ($status == "success"){
                     $resultArray = getUserFileData($userId);
@@ -23,20 +24,32 @@
                     echo $vCalendar->render();
                 }
             }else{
-                if ($isAlreadyExist[1][2] != ""){
-                    $resultArray = getUserFileData($userId);
-                    $vCalendar = geneCal($resultArray[1]);
-                    header('Content-Type: text/calendar; charset=utf-8');
-                    header('Content-Disposition: attachment; filename="Calendar'.$userId.'.ics"');
-                    echo $vCalendar->render();
+                if ($isAlreadyExist[0][0] == 0){
+                    addData($dataBase,$tableName,1,array("username"),array($userId));
+                    $status = writeNewUserArrayToFile($userId);
+                    if ($status == "success"){
+                        $resultArray = getUserFileData($userId);
+                        $vCalendar = geneCal($resultArray[1]);
+                        header('Content-Type: text/calendar; charset=utf-8');
+                        header('Content-Disposition: attachment; filename="Calendar'.$userId.'.ics"');
+                        echo $vCalendar->render();
+                    }
                 }else{
-                    $userInfo = analyseHeader(getOriginData($userId));
-                    updateData($dataBase,$tableName,"userinfo",$userInfo,1,array(),array("userid"),$userId);
-                    $resultArray = getUserFileData($userId);
-                    $vCalendar = geneCal($resultArray[1]);
-                    header('Content-Type: text/calendar; charset=utf-8');
-                    header('Content-Disposition: attachment; filename="Calendar'.$userId.'.ics"');
-                    echo $vCalendar->render();
+                    if ($isAlreadyExist[1][2] != ""){
+                        $resultArray = getUserFileData($userId);
+                        $vCalendar = geneCal($resultArray[1]);
+                        header('Content-Type: text/calendar; charset=utf-8');
+                        header('Content-Disposition: attachment; filename="Calendar'.$userId.'.ics"');
+                        echo $vCalendar->render();
+                    }else{
+                        $userInfo = analyseHeader(getOriginData($userId));
+                        updateData($dataBase,$tableName,"userinfo",$userInfo,1,array(),array("userid"),$userId);
+                        $resultArray = getUserFileData($userId);
+                        $vCalendar = geneCal($resultArray[1]);
+                        header('Content-Type: text/calendar; charset=utf-8');
+                        header('Content-Disposition: attachment; filename="Calendar'.$userId.'.ics"');
+                        echo $vCalendar->render();
+                    }
                 }
             }
         }else{
